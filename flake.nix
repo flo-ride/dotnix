596334b5fae99b24657dd8b1b1e25725321937d6
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2025-10-01 15:31:41 +0200

    build(flakes): Add sops-nix channel

--- flake.nix
+++ flake.nix
@@ -31,6 +31,10 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Secrets Management
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
     # TODO:
     # nixvim.url = "github:nix-community/nixvim";
     # nixvim.inputs.nixpkgs.follows = "nixpkgs";
