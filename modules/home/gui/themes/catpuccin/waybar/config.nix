f96b387179c87b0d764a0bcbe230d4cefa6adbee
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2025-10-19 16:20:07 +0200

    feat(theme/swaybar): Setup correct catpuccin theme

--- modules/home/gui/themes/catpuccin/waybar/config.nix
+++ modules/home/gui/themes/catpuccin/waybar/config.nix
@@ -4,8 +4,8 @@
 , ...
 }:
 {
-  # Add hyprctl to waybar env : https://github.com/hyprwm/Hyprland/issues/1835
-  # systemd.user.services.waybar.Service.Environment = "PATH=/run/wrappers/bin:${pkgs.hyprland}/bin";
+  # Add hyprctl to waybar env  = https://github.com/hyprwm/Hyprland/issues/1835
+  # systemd.user.services.waybar.Service.Environment = "PATH=/run/wrappers/bin =${pkgs.hyprland}/bin";
 
   programs.waybar.settings = {
     mainBar = {
@@ -14,143 +14,101 @@
 
       modules-left = [
         "hyprland/workspaces"
-        "custom/right-arrow-dark"
+        "custom/music"
       ];
       modules-center = [
-        "custom/left-arrow-dark"
-        "clock#1"
-        "custom/left-arrow-light"
-        "custom/left-arrow-dark"
-        "clock#2"
-        "custom/right-arrow-dark"
-        "custom/right-arrow-light"
-        "clock#3"
-        "custom/right-arrow-dark"
+        "pulseaudio"
+        "backlight"
+        "battery"
+        "clock"
       ];
       modules-right = [
-        "custom/left-arrow-dark"
         "network"
-        "custom/left-arrow-light"
-        "custom/left-arrow-dark"
-        "memory"
-        "custom/left-arrow-light"
-        "custom/left-arrow-dark"
-        "cpu"
-        "custom/left-arrow-light"
-        "custom/left-arrow-dark"
-        "battery"
-        "custom/left-arrow-light"
-        "custom/left-arrow-dark"
-        "pulseaudio"
-        "custom/left-arrow-light"
-        "custom/left-arrow-dark"
-        "tray"
+        "bluetooth"
+        "custom/lock"
+        "custom/power"
       ];
 
-      "custom/left-arrow-dark" = {
-        format = "";
-        tooltip = false;
-      };
-
-      "custom/left-arrow-light" = {
-        format = "";
-        tooltip = false;
-      };
-
-      "custom/right-arrow-dark" = {
-        format = "";
-        tooltip = false;
-      };
-
-      "custom/right-arrow-light" = {
-        format = "";
-        tooltip = false;
-      };
-
       "hyprland/workspaces" = {
-        # Enable scroll in workspaces modules
-        format = "{name}";
-        on-scroll-up = "${pkgs.hyprland}/bin/hyprctl dispatch workspace e+1";
-        on-scroll-down = "${pkgs.hyprland}/bin/hyprctl dispatch workspace e-1";
-        on-click = "activate";
-      };
-
-      "clock#1" = {
-        format = "{:%a}";
-        tooltip = false;
-      };
-      "clock#2" = {
-        format = "{:%H:%M}";
-        tooltip = false;
-      };
-      "clock#3" = {
-        format = "{:%m-%d}";
-        tooltip = false;
-      };
-
-      "pulseaudio" = {
-        format = "{icon} {volume:2}%";
-        format-bluetooth = "{icon}  {volume}%  ";
-        format-muted = "MUTE";
-        format-icons = {
-          headphones = "";
-          default = [
-            ""
-            ""
-          ];
+        "disable-scroll" = true;
+        "sort-by-name" = true;
+        "format" = " {name} ";
+        "format-icons" = {
+          "default" = "";
         };
-        scroll-step = 5;
-        on-click = "${pkgs.pamixer}/bin/pamixer -t";
-        on-click-right = "${pkgs.pavucontrol}/bin/pavucontrol";
       };
-
-      "memory" = {
-        interval = 5;
-        format = "Mem {}%";
-      };
-
-      "cpu" = {
-        "interval" = 5;
-        "format" = "CPU {usage:2}%";
-      };
-
-      "battery" = {
-        states = {
-          good = 95;
-          warning = 30;
-          critical = 15;
-        };
-        format = "{icon} {capacity}%";
-        format-icons = [
-          ""
-          ""
-          ""
-          ""
-          ""
-        ];
-      };
-
-      "disk" = {
-        interval = 5;
-        format = "Disk {percentage_used:2}%";
-        path = "/";
-      };
-
-      "tray" = {
-        icon-size = 20;
-      };
-
       "network" = {
-        interface = "wlp1s0";
-        format-wifi = " {essid} {icon} ";
-        format-icons = [
+        "format-wifi" = " {icon} {essid} ";
+        "format-icons" = [
           "󰤟"
           "󰤢"
           "󰤥"
           "󰤨"
         ];
-        format-ethernet = "{ipaddr}/{cidr} 󰈀";
-        format-disconnected = "";
+        "format-ethernet" = "󰈀 {ipaddr}/{cidr} ";
+        "format-disconnected" = "";
+      };
+
+      "bluetooth" = {
+        "format" = " ";
+        "format-disabled" = "";
+        "format-connected" = " {num_connections}";
+        "tooltip-format" = "{controller_alias}\t{controller_address}";
+        "tooltip-format-connected" = "{controller_alias}\t{controller_address}\n\n{device_enumerate}";
+        "tooltip-format-enumerate-connected" = "{device_alias}\t{device_address}";
+      };
+      "tray" = {
+        "icon-size" = 21;
+        "spacing" = 10;
+      };
+      "custom/music" = {
+        "format" = "  {}";
+        "escape" = true;
+        "interval" = 5;
+        "tooltip" = false;
+        "exec" = "${pkgs.playerctl}/bin/playerctl metadata --format='{{ title }}'";
+        "on-click" = "${pkgs.playerctl}/bin/playerctl play-pause";
+        "max-length" = 50;
+      };
+      "clock" = {
+        "timezone" = "Europe/Paris";
+        "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        "format-alt" = " {:%d/%m/%Y}";
+        "format" = " {:%H:%M}";
+      };
+      "backlight" = {
+        "device" = "intel_backlight";
+        "format" = "{icon} {percent}%";
+        "format-icons" = [ "" "" "" "" "" "" "" "" "" ];
+      };
+      "battery" = {
+        "states" = {
+          "warning" = 30;
+          "critical" = 15;
+        };
+        "format" = "{icon} {capacity}%";
+        "format-charging" = "";
+        "format-plugged" = "";
+        "format-icons" = [ "" "" "" "" "" "" "" "" "" "" "" "" ];
+      };
+      "pulseaudio" = {
+        "scroll-step" = 1;
+        "format" = "{icon} {volume}%";
+        "format-muted" = "";
+        "format-icons" = {
+          "default" = [ "" "" " " ];
+        };
+        "on-click" = "${pkgs.pavucontrol}/bin/pavucontrol";
+      };
+      "custom/lock" = {
+        "tooltip" = false;
+        "on-click" = "sh -c '(sleep 0.5s; ${pkgs.swaylock}/bin/swaylock --grace 0)' & disown";
+        "format" = "";
+      };
+      "custom/power" = {
+        "tooltip" = false;
+        "on-click" = "${pkgs.wlogout}/bin/wlogout &";
+        "format" = "襤";
       };
     };
   };
--- modules/home/gui/themes/catpuccin/waybar/default.nix
+++ modules/home/gui/themes/catpuccin/waybar/default.nix
@@ -1,9 +1,7 @@
-{
-  ...
+{ ...
 }:
 {
   imports = [ ./config.nix ];
 
   programs.waybar.style = ./style.css;
-
 }
