b6e80cdb5603842aca7d1606b29ab29ec2623542
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2025-12-16 14:30:15 +0100

    fix(nvim/clipboard): implement custom paste for windows terminal osc52
    Replace the default OSC 52 paste mechanism with a custom Lua
    function. This is a workaround for Windows Terminal, which
    does not fully support OSC 52 paste, ensuring clipboard
    operations function correctly when using Neovim over SSH.

--- modules/home/dev/nvim/lua/others/copy-paste.lua
+++ modules/home/dev/nvim/lua/others/copy-paste.lua
@@ -1,13 +1,20 @@
+function my_paste(reg)
+    return function(lines)
+        local content = vim.fn.getreg('"')
+        return vim.split(content, '\n')
+    end
+end
+
 if vim.env.SSH_TTY then
-  vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-    },
-    paste = {
-      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-    },
-  }
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            ['+'] = my_paste('+'),
+            ['*'] = my_paste('*'),
+        },
+    }
 end
