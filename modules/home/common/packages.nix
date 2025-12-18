a26d8cfe1036a2c7c664088a4de1682d14036aff
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2025-12-18 21:30:01 +0100

    feat(common): add zip and unzip support to package list
     This change enables the installation of zip and unzip programs in the package list,
     allowing users to manage these utilities alongside other dependencies.
     The added functionality expands the scope of packages that can be included in home-manager configurations.

--- modules/home/common/packages.nix
+++ modules/home/common/packages.nix
@@ -29,6 +29,9 @@ in
     # On ubuntu, we need this less for `man home-configuration.nix`'s pager to
     # work.
     less
+
+    zip
+    unzip
   ];
 
   # Programs natively supported by home-manager.
