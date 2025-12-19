fd85619c88dcaffa2428739d2ef440177f84a822
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2025-12-19 16:04:05 +0100

    refactor(nvim/lsp): restructure lsp configuration
    replaced per-server configuration with global setup and
    simplified server enabling

--- modules/home/dev/nvim/lua/lsp/lsp.lua
+++ modules/home/dev/nvim/lua/lsp/lsp.lua
@@ -1,28 +1,13 @@
-local servers = { 'ts_ls', "clangd", "jdtls", "pyright", "omnisharp", "texlab", 'volar' }
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-local on_attach = function(client, bufnr)
-    vim.api.nvim_exec_autocmds('User', {pattern = 'LspAttached'})
-    vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(args)
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            if client.server_capabilities.inlayHintProvider then
-                vim.lsp.inlay_hint.enable(args.buf, true)
-            end
-            -- whatever other lsp config you want
-        end
-    })
-end
 
+vim.lsp.config('*', {
+    root_markers = { '.git' },
+    capabilities = capabilities
+})
+
+
+local servers = { 'ts_ls', "clangd", "jdtls", "pyright", "omnisharp", "texlab" }
 for _, lsp in pairs(servers) do
-    vim.lsp.config[lsp] = {
-        on_attach = on_attach,
-        capabilities = capabilities,
-    }
+    vim.lsp.enable(lsp)
 end
-
-
--- require("mason-lspconfig").setup {
-    -- ensure_installed = servers,
--- }
