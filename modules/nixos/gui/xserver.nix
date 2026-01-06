414297f18da1a3d439ec3e76dfec712fa611d1ec
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-01-06 15:12:16 +0100

    refactor(nixos/gui): move gdm configuration to top-level services

--- modules/nixos/gui/xserver.nix
+++ modules/nixos/gui/xserver.nix
@@ -1,15 +1,14 @@
 {
   services.xserver.xkb.layout = "gb";
   services.xserver.xkb.variant = "extd";
+
+  services.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
   services.xserver = {
     enable = true;
     exportConfiguration = true;
-
-    displayManager.gdm = {
-      enable = true;
-      wayland = true;
-    };
-
   };
   services.libinput.enable = true;
 }
