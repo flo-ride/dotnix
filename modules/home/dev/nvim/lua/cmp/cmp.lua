e89f8df926a892dce9eeae19904023e7f4e7593f
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-01-07 09:32:53 +0100

    fix(nvim/cmp): fix cmp and luasnip keybindings for snippet interaction
    Adjust `<CR>`, `<Tab>`, and `<S-Tab>` mappings to correctly
    prioritize `luasnip` snippet expansion and jumping over `nvim-cmp`
    completion confirmation or selection. This fixes conflicts and ensures
    a smoother workflow when using both plugins.

--- modules/home/dev/nvim/lua/cmp/cmp.lua
+++ modules/home/dev/nvim/lua/cmp/cmp.lua
@@ -1,12 +1,13 @@
 -- Set up nvim-cmp.
-local cmp = require'cmp'
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 
 cmp.setup({
     snippet = {
         -- REQUIRED - you must specify a snippet engine
         expand = function(args)
             -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
             -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
             -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
             -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
@@ -25,32 +26,41 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true },
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        ['<CR>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
+                if luasnip.expandable() then
+                    luasnip.expand()
+                else
+                    cmp.confirm({
+                        select = true,
+                    })
+                end
             else
                 fallback()
             end
+        end),
+       ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.locally_jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
         end, { "i", "s" }),
         ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
         end, { "i", "s" }),
   --    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
   --    ['<C-f>'] = cmp.mapping.scroll_docs(4),
   --    ['<C-Space>'] = cmp.mapping.complete(),
-  --    ['<C-e>'] = cmp.mapping.abort(),
-  --    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --    ['<C-e>'] = cmp.mapping.abort(),  --    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
