09bb02352fb8e728f549d859047702e2e96cd0a9
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-01-28 11:44:16 +0100

    feat(vm): configure kvmfr and spice usb redirection
    Enable spice USB redirection and configure KVM FrameRelay (kvmfr) support for Looking Glass.
    This includes:
    - Adding the `kvmfr` kernel module and setting static size.
    - Configuring udev rules and tmpfiles for permissions on `/dev/kvmfr0` and shared memory.
    - Updating libvirtd qemu configuration to whitelist `/dev/kvmfr0`.

--- modules/nixos/vm.nix
+++ modules/nixos/vm.nix
@@ -1,15 +1,41 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.virt-manager.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       package = pkgs.qemu_kvm;
       runAsRoot = true;
-      swtpm.enable = true; # Required for Windows 11 (TPM support)
+      swtpm.enable = true;
     };
   };
-  programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
     looking-glass-client
   ];
+
+  systemd.tmpfiles.rules = [
+    "f /dev/shm/looking-glass 0660 floride libvirtd -"
+  ];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="kvmfr", OWNER="floride", GROUP="kvm", MODE="0660"
+  '';
+
+  boot.extraModulePackages = [config.boot.kernelPackages.kvmfr];
+  boot.kernelModules = ["kvmfr"];
+  boot.extraModprobeConfig = ''
+    options kvmfr static_size_mb=64
+  '';
+  virtualisation.libvirtd.qemu.verbatimConfig = ''
+    cgroup_device_acl = [
+        "/dev/null", "/dev/full", "/dev/zero",
+        "/dev/random", "/dev/urandom",
+        "/dev/ptmx", "/dev/kvm",
+        "/dev/kvmfr0"
+    ]
+  '';
 }
