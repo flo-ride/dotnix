c8343b71db5e1322589c84599fdffef7c1d6f2af
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-03-31 12:24:56 +0200

    fix(nixos/common/network): allow dnssec downgrade in resolved
    Changed DNSSEC validation mode from strict enforcement to allowing-downgrade, enabling compatibility with servers that don't support DNSSEC validation.

--- modules/nixos/common/network_dns.nix
+++ modules/nixos/common/network_dns.nix
@@ -1,7 +1,7 @@
 {...}: {
   services.resolved = {
     enable = true;
-    dnssec = "true";
+    dnssec = "allow-downgrade";
     dnsovertls = "opportunistic";
     fallbackDns = ["9.9.9.9#dns.quad9.net" "1.1.1.1#cloudflare-dns.com"];
   };
