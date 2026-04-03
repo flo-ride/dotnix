9988026bf4e17b96fdebefe0ba60accce5137795
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-04-03 15:13:49 +0200

    feat(nixos/common/network): add trusted interface configuration to firewall
    Add local loopback interface to trusted interfaces in firewall configuration.
    This allows traffic between localhost and system services on all network stacks.

--- modules/nixos/common/network.nix
+++ modules/nixos/common/network.nix
@@ -1,6 +1,7 @@
 {lib, ...}: {
   networking.firewall = {
     enable = lib.mkDefault true;
+    trustedInterfaces = ["lo"];
     allowPing = lib.mkDefault false; # Plus discret sur le réseau
     logReversePathDrops = true; # Log les paquets suspects pour l'audit
   };
