cf48a57dfda622801e7130f9b3004242e3335117
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-07-08 15:49:29 +0200

    chore(nixos/gui): remove gdm disable configuration
    Remove the explicit configuration block that disabled the GDM display manager.

--- modules/nixos/gui/xserver.nix
+++ modules/nixos/gui/xserver.nix
@@ -2,9 +2,6 @@
   services.xserver.xkb.layout = "gb";
   services.xserver.xkb.variant = "extd";
 
-  services.displayManager.gdm = {
-    enable = false;
-  };
   services.xserver = {
     enable = true;
     exportConfiguration = true;
