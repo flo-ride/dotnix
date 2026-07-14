ba53d3e348743e8ac1eca2077b4ffd4602314793
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-07-14 16:01:38 +0200

    feat(nixos/games): enable protontricks

--- modules/nixos/games/steam.nix
+++ modules/nixos/games/steam.nix
@@ -13,6 +13,7 @@ in {
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    protontricks.enable = true;
   };
   programs.gamemode.enable = true;
 
