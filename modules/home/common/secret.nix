84919c6aa3a01cdcf283f4a201ebb24356a3d98c
Flo Ride <43076999+flo-ride@users.noreply.github.com>
2026-07-22 12:49:06 +0200

    chore(sops): update age keys and sops configuration
    Update the floride age key and reformat the users list in .sops.yaml.
    Transition home/common/secret.nix to use keyFile instead of sshKeyPaths.
    Add new secrets for common SSH and Forgejo public keys.
    Configure sops-nix to allow non-zero exit statuses to facilitate bootstrapping.
    Update nixos/common/security/secret.nix to include sshKeyPaths.
    Initialize secrets.yaml with new encrypted values.

--- modules/home/common/secret.nix
+++ modules/home/common/secret.nix
@@ -12,5 +12,18 @@ in {
   ];
 
   sops.defaultSopsFile = ../../../secrets.yaml;
-  sops.age.sshKeyPaths = ["/home/floride/.ssh/id_flo"];
+  sops.age.keyFile = "/home/floride/.config/sops/age/keys.txt";
+
+  sops.secrets."ssh/common_ssh_pub" = {
+    path = "/home/floride/.ssh/common_ssh.pub";
+  };
+  sops.secrets."ssh/forgejo_floride_pub" = {
+    path = "/home/floride/.ssh/forgejo_floride.pub";
+  };
+
+  # Allow sops-nix to fail without crashing the whole home-manager activation.
+  # Useful for bootstrapping a new machine where the age key is not yet available.
+  systemd.user.services.sops-nix = {
+    Service.SuccessExitStatus = [ 0 1 2 127 ];
+  };
 }
--- modules/nixos/common/security/secret.nix
+++ modules/nixos/common/security/secret.nix
@@ -11,5 +11,8 @@ in {
     inputs.sops-nix.nixosModules.sops
   ];
 
-  sops.defaultSopsFile = ../../../../secrets.yaml;
+  sops = {
+    defaultSopsFile = ../../../../secrets.yaml;
+    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  };
 }
